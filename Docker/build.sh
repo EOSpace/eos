806fe6b9d365c9809f8b22be626410e3730a5ca6
bytemaster <tyeenoprom@gmail.com>
2019-06-23 18:02:25 +0800

    update dockerfile

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TAG=v1.5.2
+TAG=v1.7.3
 BRANCH=eospace/feature/kafka_plugin
 nohup docker build -t eospace/eos:$TAG -f Dockerfile.custom --build-arg branch=$BRANCH --build-arg symbol=EOS . > build.log &
